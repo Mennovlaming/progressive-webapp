cff9df7f5923b9b00f4f97e20919bc93bd6cd65b
Menno Vlaming <mennovlaming@gmail.com>
2023-03-20 10:54:00 +0100

    Add files via upload

---
+++ modules/articleHandler.js
@@ -0,0 +1,37 @@
+import $ from "./querySelector.js"
+
+export const addHashEvent = () => {
+    const onHashChangeHandler = (e) => {
+        // removes #
+        //                "#test123".substring() =
+        const articleId = location.hash.substring(1,location.hash.length);
+
+        const article = window.articles.find(x => x.id == articleId);
+        if(article) {
+            const section = $('body');
+            section.insertAdjacentHTML ("beforeend", `
+            <div id="article-screen">
+           
+            <article>
+                <h2>${article.title}</h2>
+                <h3>${article.principalOrFirstMaker}</h3>
+                <!-- <p>${article.productionPlaces}</p> -->
+                
+                <img src="${article.webImage.url}" />
+
+            </article>
+            <button type="button" onclick="removeArticleScreen()">
+                <img src="/images/cancel.png" />
+            </button>
+            </div>
+            `);
+        }
+        };
+        // als de hash veranderd, trigger onHashChangeHandler
+    window.onhashchange = onHashChangeHandler;
+}
+
+//zoek id article screen, remove die.
+window.removeArticleScreen = () => {
+    document.getElementById("article-screen").remove();
+}
